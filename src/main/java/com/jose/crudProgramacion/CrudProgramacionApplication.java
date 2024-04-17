53c0d3d061ea2e8f5415f72dc16585a96ee4ec8a
Miguel2022104 <miguel.martinez@unac.edu.co>
2024-04-16 21:50:19 -0500

    :zap: desarroolo

---
+++ src/main/java/com/jose/crudProgramacion/CrudProgramacionApplication.java
@@ -0,0 +1,13 @@
+package com.jose.crudProgramacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudProgramacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudProgramacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jose/crudProgramacion/CrudProgramacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jose.crudProgramacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProgramacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
